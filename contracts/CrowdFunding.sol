55c578451b542c25fc5cc35458bd3f3e3d9d1718
andrescrd <c.andresricardo@gmail.com>
2021-11-05 17:12:11 -0300

    add create request

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -10,11 +10,70 @@ contract CrowdFunding{
     uint public goal;
     uint public raisedAmount;
 
+    struct Request {
+        string description;
+        address payable beneficiary;
+        uint amount;
+        bool completed;
+        uint numVotes;
+        mapping(address => bool) voted;
+    }
+
+    uint numRequests;   
+    mapping(uint => Request) public requests;
+
     constructor(address _admin, uint _minContribution, uint _deadline, uint _goal){
         admin = _admin;
         minContribution = _minContribution;
         deadline = block.timestamp + _deadline;
         goal = _goal;
         raisedAmount = 0;
+    }
+
+    function contribute() public payable {
+        require(block.timestamp < deadline, 'Campaign has ended!');
+        require(msg.value >= minContribution, 'Contribution is less than minimum!');
+
+        if(contributors[msg.sender] == 0){
+            noOfContributors++;
+        }
+
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+
+    receive() payable external{
+        contribute();
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function getRefaund() public {
+        require(block.timestamp > deadline && raisedAmount < goal, 'Campaign has ended!');
+        require(contributors[msg.sender] > 0, 'You have not contributed!');
+
+        address payable recipient = payable(msg.sender);
+        uint refund = contributors[msg.sender];
+        recipient.transfer(refund);
+
+        contributors[msg.sender] = 0;
+    }
+
+    function createRequest(string memory _description, address payable _beneficiary, uint _amount) public onlyAdmin {
+        Request storage request = requests[numRequests];
+        numRequests++;
+
+        request.description = _description;
+        request.beneficiary = _beneficiary;
+        request.amount = _amount;
+        request.completed = false;
+        request.numVotes = 0;
+    }
+
+    modifier onlyAdmin{
+        require(msg.sender == admin, 'Only admin can call this function!');
+        _;
     }
 }
